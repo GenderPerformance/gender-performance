27904ab0ab4f07f4487f90430e967074c09561ea
Yin Song <songniy@gmail.com>
2021-03-17 18:41:15 -0400

    travis testing

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
-const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
+const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}` : pkg.name
 const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
 let config
 if (process.env.DATABASE_URL) {
