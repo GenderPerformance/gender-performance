1cff818c113a37d8d1547bd0d84d412f6d079012
Yin Song <songniy@gmail.com>
2021-03-17 20:19:43 -0400

    testing : added ml folder trying to integrate

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,40 @@
 const router = require('express').Router()
+const {exec} = require('child_process')
 module.exports = router
 
 router.use('/users', require('./users'))
+
+const fileDir = '../../gendervoicemodel/test-samples/cloudbufferin.wav'
+
+let realResult
+
+async function getPrediction() {
+  return await exec(
+    `python ../../gendervoicemodel/test.py --file ${fileDir}`,
+    (error, data, getter) => {
+      if (error) {
+        console.log('error', error.message)
+        return
+      }
+      if (getter) {
+        console.log('data', data)
+        realResult = data
+        return
+      }
+      console.log('data', data)
+    }
+  )
+}
+
+router.get('/predictions', async (req, res, next) => {
+  try {
+    await getPrediction()
+    let result = realResult
+    res.json(result)
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
