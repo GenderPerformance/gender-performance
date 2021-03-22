b51819b0bb4d3b840811033bc3aacc5b85caa236
Daniel Poveda <dlpoveda@gmail.com>
2021-03-22 18:21:09 -0400

    Feat: Material UI

--- client/components/Analysis.js
+++ client/components/Analysis.js
@@ -1,71 +1,22 @@
 import React from 'react'
-import AudioReactRecorder, {RecordState} from 'audio-react-recorder'
-import {Link} from 'react-router-dom'
+import {Container, Card} from '@material-ui/core'
 
 export default class Recorder extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      recordState: null,
-      audioData: null
-    }
-  }
-
-  start = () => {
-    this.setState({
-      recordState: RecordState.START
-    })
-  }
-
-  stop = () => {
-    this.setState({
-      recordState: RecordState.STOP
-    })
-  }
-
-  pause = () => {
-    this.setState({
-      recordState: RecordState.PAUSE
-    })
-  }
-
-  //audioData contains blob and blobUrl
-  onStop = audioData => {
-    console.log('audioData', audioData)
-    this.setState({audioData: audioData})
-  }
-
   render() {
-    const {recordState} = this.state
-
     return (
-      <div>
-        <AudioReactRecorder state={recordState} onStop={this.onStop} />
-
+      <Container maxWidth="sm">
         <div>
-          <audio
-            id="audio"
-            controls
-            src={this.state.audioData ? this.state.audioData.url : null}
-          />
           <br />
-          <button type="button" id="record" onClick={this.start}>
-            Start
-          </button>
-          <button type="button" id="pause" onClick={this.pause}>
-            Pause
-          </button>
-          <button type="button" id="stop" onClick={this.stop}>
-            Stop
-          </button>
-          <Link to="/analysis">
-            <button type="button" id="analysis">
-              Analyze
-            </button>
-          </Link>
+          <br />
+          <Card style={{backgroundColor: '#cbae82'}}>
+            <h4>Analysis</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consecteturadipiscing elit. Donec
+              viverra eget nullavitae ornare.
+            </p>
+          </Card>
         </div>
-      </div>
+      </Container>
     )
   }
 }
