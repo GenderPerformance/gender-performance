276a57d26867c5b933a3a918b377289d96c61560
Yin Song <songniy@gmail.com>
2021-03-25 12:13:25 -0400

    analysis dynamically shows up if there is a recording

--- client/components/Analysis.js
+++ client/components/Analysis.js
@@ -3,20 +3,28 @@ import {Container, Card} from '@material-ui/core'
 
 export default class Recorder extends React.Component {
   render() {
-    return (
-      <Container maxWidth="sm">
-        <div>
-          <br />
-          <br />
-          <Card style={{backgroundColor: '#cbae82'}}>
-            <h4>Analysis</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consecteturadipiscing elit. Donec
-              viverra eget nullavitae ornare.
-            </p>
-          </Card>
-        </div>
-      </Container>
-    )
+    if (!this.props.recording) {
+      return <div>Loading...</div>
+    } else {
+      const wavesurfer = WaveSurfer.create({
+        container: '#waveform'
+      })
+      wavesurfer.load(this.props.recording.recordingURL)
+      return (
+        <Container maxWidth="sm">
+          <div>
+            <br />
+            <br />
+            <Card style={{backgroundColor: '#cbae82'}}>
+              <h4>Analysis</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consecteturadipiscing elit. Donec
+                viverra eget nullavitae ornare.
+              </p>
+            </Card>
+          </div>
+        </Container>
+      )
+    }
   }
 }
