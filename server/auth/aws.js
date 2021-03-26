29550ea01625a6887a48b39d44bcc36a5775a06b
Yin Song <songniy@gmail.com>
2021-03-26 14:59:28 -0400

    fix: updated conditional for aws secrets

--- server/auth/aws.js
+++ server/auth/aws.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const aws = require('aws-sdk')
-if (process.env.NODE_ENV !== 'production') {
+console.log(process.env)
+//for PC
+if (process.env.PGHOST === 'localhost') {
+  //for macs
+  //if (process.env.NODE_ENV !== 'production') {
   require('../../secrets')
 }
 const S3_BUCKET = process.env.S3_BUCKET
