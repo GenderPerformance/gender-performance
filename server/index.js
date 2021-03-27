026f650890f5d35606d80eeec0b2f8b4f5c785a1
Yin Song <songniy@gmail.com>
2021-03-27 17:03:18 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -26,8 +26,12 @@ if (process.env.NODE_ENV === 'test') {
  * keys as environment variables, so that they can still be read by the
  * Node process on process.env
  */
-if (process.env.PGHOST === 'localhost' || process.env.NODE_ENV !== 'production')
+if (
+  process.env.PGHOST === 'localhost' ||
+  process.env.NODE_ENV !== 'production'
+) {
   require('../secrets')
+}
 //for macs
 //if (process.env.NODE_ENV !== 'production') require('../secrets')
 // passport registration
