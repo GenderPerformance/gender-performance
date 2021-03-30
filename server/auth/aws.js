ac54865ddc1755febfa60a918e65429c0688138e
Yin Song <songniy@gmail.com>
2021-03-29 21:21:15 -0400

    fix: added conditionals for requiring secrets so pc unix and heroku and work properly

--- server/auth/aws.js
+++ server/auth/aws.js
@@ -1,10 +1,8 @@
 const router = require('express').Router()
 const aws = require('aws-sdk')
 
-//for PC
-//if (process.env.PGHOST === 'localhost') {
-//for macs
-if (process.env.NODE_ENV !== 'production') {
+
+if ((process.env.PGHOST === 'localhost'||process.env.NODE_ENV !== 'production')&&!process.env.TRAVIS) {
   require('../../secrets')
 }
 const S3_BUCKET = process.env.S3_BUCKET
