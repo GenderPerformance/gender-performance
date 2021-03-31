67b50dded21fdf7ae5b22f9e058f4ccf61c144c6
Daniel Poveda <dlpoveda@gmail.com>
2021-03-31 16:15:23 -0400

    Chore: redo frontend

--- client/components/Recorder.js
+++ client/components/Recorder.js
@@ -54,89 +54,69 @@ class Recorder extends React.Component {
       return <div>Loading...</div>
     } else {
       return (
-        <Container maxWidth="sm">
-          <br />
-          <Card
-            style={{
-              backgroundColor: '#cbae82',
-              paddingLeft: '2em',
-              paddingRight: '2em'
-            }}
-          >
-            <h1>Performance</h1>
-          </Card>
-          <br />
-          <Card
-            style={{
-              backgroundColor: '#ffe0b2'
-            }}
-          >
-            <div className="recorder">
-              <h4>RECORD</h4>
-              <AudioReactRecorder
-                text-align="center"
-                state={recordState}
-                onStop={this.onStop}
-                backgroundColor="rgb(255,224,177)"
-                foregroundColor="rgb(151,180,151)"
-                canvasHeight="100"
-              />
-              <audio
-                id="audio"
-                controls
-                src={recordingURL ? recordingURL : null}
-              />
-              <br />
-              <ButtonGroup
-                variant="contained"
-                color="secondary"
-                aria-label="contained primary button group"
-              >
-                <Button
-                  size="small"
-                  type="button"
-                  id="record"
-                  onClick={this.start}
-                >
-                  Record
-                </Button>
-                <Button type="button" id="stop" onClick={this.stop}>
-                  Stop
-                </Button>
-                {this.state.blob ? (
-                  <Link component={RouterLink} to="/Analysis" variant="button">
-                    <Button
-                      style={{
-                        backgroundColor: '#c8e6c8'
-                      }}
-                      type="button"
-                      id="analysis"
-                      onClick={() => {
-                        this.props.analyzeClip(userId, recordingBlob)
-                        this.setState({blob: null})
-                      }}
-                    >
-                      Analyze
-                    </Button>
-                  </Link>
-                ) : null}
-              </ButtonGroup>
-              <br />
-            </div>
-          </Card>
-          <br />
-          <Card style={{backgroundColor: '#c8e6c7'}}>
-            <div className="txtgen">
-              Press record then say:
-              <h4>{`${this.state.paragraph}`}</h4>
+        <Container className="recordPage">
+          <Container className="recordButtonParagraph">
+            <ButtonGroup
+              variant="contained"
+              color="secondary"
+              aria-label="contained primary button group"
+            >
               <Button
-                variant="contained"
-                color="primary"
-                onClick={this.newParagraph}
+                size="small"
+                type="button"
+                id="record"
+                onClick={this.start}
               >
-                New Paragraph
+                Record
               </Button>
-            </div>
+              <Button type="button" id="stop" onClick={this.stop}>
+                Stop
+              </Button>
+              {this.state.blob ? (
+                <Link component={RouterLink} to="/Analysis" variant="button">
+                  <Button
+                    style={{
+                      backgroundColor: '#c8e6c8'
+                    }}
+                    type="button"
+                    id="analysis"
+                    onClick={() => {
+                      this.props.analyzeClip(userId, recordingBlob)
+                      this.setState({blob: null})
+                    }}
+                  >
+                    Analyze
+                  </Button>
+                </Link>
+              ) : null}
+            </ButtonGroup>
+            <Card style={{backgroundColor: '#c8e6c7'}}>
+              <div className="txtgen">
+                Press record then say:
+                <h4>{`${this.state.paragraph}`}</h4>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.newParagraph}
+                >
+                  New Paragraph
+                </Button>
+              </div>
+            </Card>
+          </Container>
+          <Card>
+            <AudioReactRecorder
+              state={recordState}
+              onStop={this.onStop}
+              backgroundColor="rgb(255,224,177)"
+              foregroundColor="rgb(151,180,151)"
+              canvasHeight="100"
+            />
+            <audio
+              id="audio"
+              controls={recordingURL}
+              src={recordingURL ? recordingURL : null}
+            />
           </Card>
         </Container>
       )
