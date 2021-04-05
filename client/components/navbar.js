bc7e99d3d718a7959fd1e901494190d7347ee724
jponto <40177978+jponto@users.noreply.github.com>
2021-04-05 16:21:38 -0400

    Revert "April5"

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import {AppBar} from '@material-ui/core'
-import MenuBar from './MenuBar';
-import {Link} from 'react-router-dom';
+import MenuBar from './MenuBar'
 
 const Navbar = () => (
   <div>
     <nav>
         <AppBar position="fixed">
           <div className="Menu">
-            <Link to="/home"><div className="title">
-              <img src={`/waves/wave4.png`} id="titleicon"/>
-              <h1 id="navtitle">  VisiVox</h1>
             <div className="title">
               <h1>VisiVox</h1>
             </div>
-            </Link>
             <div>
               <MenuBar />
             </div>
